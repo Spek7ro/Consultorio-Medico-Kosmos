94f20b09e7852ba7987f64a5e273b0e028eface5
Cristian <71907925+Spek7ro@users.noreply.github.com>
2025-05-09 14:37:43 -0600

    add project

---
+++ src/main/java/com/kosmos/consultorios_medicos/ConsultoriosMedicosApplication.java
@@ -0,0 +1,13 @@
+package com.kosmos.consultorios_medicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultoriosMedicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultoriosMedicosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kosmos/consultorios_medicos/ConsultoriosMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmos.consultorios_medicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoriosMedicosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
